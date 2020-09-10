451371166e176cab25fb0b918440e59f903d8d29
platoneko <platonekosama@gmail.com>
2020-09-10 22:20:13 +0800

    Write in one txn

--- kv/storage/standalone_storage/standalone_storage.go
+++ kv/storage/standalone_storage/standalone_storage.go
@@ -43,27 +43,23 @@ func (s *StandAloneStorage) Reader(ctx *kvrpcpb.Context) (storage.StorageReader,
 
 func (s *StandAloneStorage) Write(ctx *kvrpcpb.Context, batch []storage.Modify) error {
 	// Your Code Here (1).
+	txn := s.kvDB.NewTransaction(true)
 	for _, m := range batch {
 		switch m.Data.(type) {
 		case storage.Put:
 			put := m.Data.(storage.Put)
-			txn := s.kvDB.NewTransaction(true)
 			if err := txn.Set(engine_util.KeyWithCF(put.Cf, put.Key), put.Value); err != nil {
-				return err
-			}
-			if err := txn.Commit(); err != nil {
 				return err
 			}
 		case storage.Delete:
 			del := m.Data.(storage.Delete)
-			txn := s.kvDB.NewTransaction(true)
 			if err := txn.Delete(engine_util.KeyWithCF(del.Cf, del.Key)); err != nil {
 				return err
 			}
-			if err := txn.Commit(); err != nil {
-				return err
-			}
 		}
+	}
+	if err := txn.Commit(); err != nil {
+		return err
 	}
 	return nil
 }
